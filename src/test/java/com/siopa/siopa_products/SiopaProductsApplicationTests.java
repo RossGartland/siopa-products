d6eb5558bf6014f001045fd34567194a47569aed
Ross Gartland <rgartland117@hotmail.com>
2025-02-24 19:05:51 +0000

    first commit

---
+++ src/test/java/com/siopa/siopa_products/SiopaProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siopa.siopa_products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiopaProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
